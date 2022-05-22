2f857b667a9a47cc0a007c951c4a4a8c20c67828
Orestis <mar.orestis@gmail.com>
2022-05-22 10:26:05 +0100

    Add directories module

---
+++ Python/directories.py
@@ -0,0 +1,23 @@
+"""
+Summary:
+List of frequently used directories
+
+
+Created by maror at 20/02/2021
+"""
+
+import os
+
+_C_DIR = 'C:\\'
+_TMP = os.path.join(_C_DIR, 'temp')
+
+BETTING_TESTS = os.path.join(_TMP, 'betting_tests')
+FUNPLACE = os.path.join(_C_DIR, 'git', 'fun_place')
+PYTHON_FUNPLACE = os.path.join(FUNPLACE, 'Python')
+W_DOWNLOADS = 'C:\\Users\\maror\\Downloads'
+
+LOG_DIR = os.path.join(_C_DIR, 'Python_logs')
+
+BETTING_DATA_DIR = os.path.join(_C_DIR, 'Data', 'Betting')
+
+
